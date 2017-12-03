b2f75ff4c2d1b4d4489fe940eb0155fe2f391a1b
ErikBoesen <me@erikboesen.com>
2017-12-02 19:54:50 -0500

    Improve code cleanliness

--- NYTSentiment.py
+++ NYTSentiment.py
@@ -1,29 +1,23 @@
-import sys
-import os
-import time
-import requests
-import nltk
-import json
 import csv
 from SentimentIngress import *
 
-#ask for a term to search
-searchTerm = input("What do you want to analyze?")
+searchTerm = input('What do you want to analyze?')
 
-#make a request to the new york times api
+# Make request to NYT's API
 dataSetRaw = makeJsonRequest(searchTerm)
 
-#access the headlines and store them in a list
-response = dataSetRaw["response"]["docs"][0]['headline']["main"]
-headlines= []
-for item in dataSetRaw["response"]["docs"]:
-    headlines.append([item['headline']["main"]])
+# access the headlines and store them in a list
+response = dataSetRaw['response']['docs'][0]['headline']['main']
+headlines = []
+for item in dataSetRaw['response']['docs']:
+    headlines.append([item['headline']['main']])
 
-#write them into a csv file
-with open("headlines.csv", 'w') as headlineFile:
-    writer = csv.writer(headlineFile, quoting=csv.QUOTE_ALL, lineterminator='\n')
+# write them into a csv file
+with open('headlines.csv', 'w') as headlineFile:
+    writer = csv.writer(headlineFile, quoting=csv.QUOTE_ALL,
+                        lineterminator='\n')
     for headline in headlines:
         writer.writerow(headline)
 
-#analyze the sentiment of each headline given by the search term
+# analyze the sentiment of each headline given by the search term
 AnalyzeSentiment('headlines.csv')
--- SentimentIngress.py
+++ SentimentIngress.py
@@ -1,6 +1,6 @@
-#NYTSentiment
-#Sentiment analysis for the NYT
-#By Dieter Brehm
+# NYTSentiment
+# Sentiment analysis for the NYT
+# By Dieter Brehm
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -8,71 +8,68 @@ from nltk.stem.porter import PorterStemmer
 import requests
 import nltk
 import pandas as pd
-import numpy as np
 import re
-import json
+
 
 def makeJsonRequest(queryText):
     search = queryText
-    TimesKey = ""
-    url="http://api.nytimes.com/svc/search/v2/articlesearch.json"
-    query_params={'q' : search,
-                  'api-key': TimesKey,
-         }
+    TimesKey = ''
+    url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json'
+    query_params = {
+        'q': search,
+        'api-key': TimesKey,
+    }
     request = requests.get(url, params=query_params)
     data = request.json()
     return data
 
-def AnalyzeSentiment(testDataFile, ):
-    #should be a csv file
-    #pandas really likes csv files for use in dataframes
-    testDataPath = testDataFile
-
-    #load in a dataset into both testing and training data frames
-    test_data_frame = pd.read_csv(testDataPath, header=None, delimiter="\t", quoting=3 )
-    test_data_frame.columns = ["Text"]
-    train_data_frame = pd.read_csv('original_train_data.csv', header=None, delimiter="\t", quoting=3 )
-    train_data_frame.columns = ["Sentiment", "Text"]
-
-    #strip all the useless punctuation and other unnecessary information
-    stemmer = PorterStemmer()
-    def stem_tokens(tokens, stemmer):
-        stemmed = []
-        for item in tokens:
-            stemmed.append(stemmer.stem(item))
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
         return stemmed
 
-    #make the words seperate and further remove useless items
-    def tokenize(text):
-        text = re.sub("[^a-zA-Z]", " ", text)
-        #tokens are basically seperate instances of words
-        #with them we can do things like count instances of specific words
-        #or graph average number of words per row(if there are multiple rows of text :P
-        tokens = nltk.word_tokenize(text)
-        stems = stem_tokens(tokens, stemmer)
-        return stems
+def tokenize(text):
+    stemmer = PorterStemmer()
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    # tokens are basically seperate instances of words
+    # with them we can do things like count instances of specific words
+    # or graph average number of words per row(if there are multiple rows of text :P
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
 
-    #take our tokenize function and use it on our training data set
+def AnalyzeSentiment(testDataFile):
+    # should be a csv file
+    # pandas really likes csv files for use in dataframes
+    testDataPath = testDataFile
+
+    # load in a dataset into both testing and training data frames
+    test_data_frame = pd.read_csv(testDataPath, header=None, delimiter='\t', quoting=3)
+    test_data_frame.columns = ['Text']
+    train_data_frame = pd.read_csv('original_train_data.csv', header=None, delimiter='\t', quoting=3)
+    train_data_frame.columns = ['Sentiment', 'Text']
+
+    # Use our tokenize function on training dataset
     vectorizer = CountVectorizer(
-        analyzer= 'word',
-        tokenizer= tokenize,
-        lowercase= True,
-        stop_words= 'english',
-        max_features= 85
+        analyzer='word',
+        tokenizer=tokenize,
+        lowercase=True,
+        stop_words='english',
+        max_features=85
     )
 
-    #combine all the data we need to look at, training and to be predicted, into one collection
+    # Combine all the data we need to look at, training and to be predicted, into one collection
     fit_data = vectorizer.fit_transform(train_data_frame.Text.tolist() + test_data_frame.Text.tolist())
     fit_data = fit_data.toarray()
 
-    #predict the sentiment of unlabeled data
+    # Predict the sentiment of unlabeled data
     model = LogisticRegression()
     model = model.fit(X=fit_data[0:len(train_data_frame)], y=train_data_frame.Sentiment)
     pred = model.predict(fit_data[len(train_data_frame):])
 
-    #read each row of the text column along with the predicted sentiment
-    #1 is positive
-    #0 is negative
+    # read each row of the text column along with the predicted sentiment
+    # 1 => positive
+    # 0 => negative
     for text, sentiment in zip(test_data_frame.Text, pred):
         print(sentiment, text)
-
